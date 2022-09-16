f89299da3fdc16f3afbbb0dfc2bb05d234d7549b
NiklasOPF <nikfor@student.chalmers.se>
2022-09-16 06:50:57 +0200

    added double linear utility function

--- UtilityFunctions/UtilityFunction.py
+++ UtilityFunctions/UtilityFunction.py
@@ -15,8 +15,8 @@ class UtilityFunction(ABC):
 
 """"
 Deifines a linear utility function of the form:
-        y = x * a/(2b)      if x<2b
-        y = a               if x>2b
+        y = x * a/ b      if x<b
+        y = a               if x>b
 """
 
 class LinearUtilityFunction(UtilityFunction):
@@ -30,12 +30,35 @@ class LinearUtilityFunction(UtilityFunction):
     def GetUtility(self, hours):
         if hours < 0:
             return 0
-        if hours < 2 * self.b:
-            return hours * self.a / (2 * self.b)
+        if hours < self.b:
+            return hours * self.a / self.b
         return self.a
 
     def GetUtilityFunctionType(self):
         return "Linear"
+
+    def GetPerformanceType(self):
+        return self.performanceType
+
+class DoubleLinearUtilityFunction(UtilityFunction):
+    def __init__(self, params, performanceType):
+        self.a1 = params[0]
+        self.b1 = params[1]
+        self.a2 = params[2]
+        self.b2 = params[3]
+        self.performanceType = performanceType
+
+    def GetUtility(self, hours):
+        if hours < 0:
+            return 0
+        if hours < self.b1:
+            return hours * self.a / self.b
+        if hours < self.b2:
+            self.a1 + (self.a2 - self.a1) / (self.b2 - self.b1) * (hours - self.b1)
+        return self.a2
+
+    def GetUtilityFunctionType(self):
+        return "DoubleLinear"
 
     def GetPerformanceType(self):
         return self.performanceType
