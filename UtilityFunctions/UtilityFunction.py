64f5913a637650f0f5115fd07586b5e61a94ebcf
NiklasOPF <nikfor@student.chalmers.se>
2022-09-22 19:26:30 +0200

    added rigoruos testing for the utility functions

--- UtilityFunctions/UtilityFunction.py
+++ UtilityFunctions/UtilityFunction.py
@@ -26,10 +26,20 @@ class ScalingUtilityFunction(UtilityFunction):
     '''' THis constructor exrects list parameters '''
 
     def __init__(self, param, performanceType):
+        if len(param) != 1:
+            raise ValueError("There must be one input parameter")
+        if np.isnan(param[0]) or param[0] < 0:
+            raise ValueError("Input parameter a must be a positive number")
+
         self.a = param[0]
         self.performanceType = performanceType
 
     def GetUtility(self, x):
+        if np.isnan(x):
+            raise ValueError("The utility function input cannot be null")
+        if x < 0:
+            raise ValueError("The utility function only accepts positive values")
+
         if np.isnan(x):
             return 0
         if x < 0:
@@ -43,14 +53,26 @@ class ScalingUtilityFunction(UtilityFunction):
         return self.performanceType
 
 class LinearUtilityFunction(UtilityFunction):
-    '''' THis constructor exrects list parameters '''
+    '''' THis constructor expects list parameters '''
 
     def __init__(self, params, performanceType):
+        if len(params) != 2:
+            raise ValueError("There must be two input parameters")
+        if np.isnan(params[0]) or params[0] < 0:
+            raise ValueError("Input parameter a must be a positive number")
+        if np.isnan(params[1]) or params[1] < 0:
+            raise ValueError("Input parameter b must be a positive number")
+
         self.a = params[0]
         self.b = params[1]
         self.performanceType = performanceType
 
     def GetUtility(self, x):
+        if np.isnan(x):
+            raise ValueError("The utility function input cannot be null")
+        if x < 0:
+            raise ValueError("The utility function only accepts positive values")
+
         if np.isnan(x):
             return 0
         if x < 0:
@@ -67,6 +89,19 @@ class LinearUtilityFunction(UtilityFunction):
 
 class DoubleLinearUtilityFunction(UtilityFunction):
     def __init__(self, params, performanceType):
+        if len(params) != 4:
+            raise ValueError("There must be four input parameters")
+        if np.isnan(params[0]) or params[0] < 0:
+            raise ValueError("Input parameter a1 must be a positive number")
+        if np.isnan(params[1]) or params[1] < 0:
+            raise ValueError("Input parameter b1 must be a positive number")
+        if np.isnan(params[2]) or params[2] < 0:
+            raise ValueError("Input parameter a2 must be a positive number")
+        if np.isnan(params[3]) or params[3] < 0:
+            raise ValueError("Input parameter b2 must be a positive number")
+        if params[1] > params[3]:
+            raise ValueError("Input parameter b2 must be larger than b1")
+
         self.a1 = params[0]
         self.b1 = params[1]
         self.a2 = params[2]
@@ -75,13 +110,18 @@ class DoubleLinearUtilityFunction(UtilityFunction):
 
     def GetUtility(self, x):
         if np.isnan(x):
+            raise ValueError("The utility function input cannot be null")
+        if x < 0:
+            raise ValueError("The utility function only accepts positive values")
+
+        if np.isnan(x):
             return 0
         if x < 0:
             return 0
         if x < self.b1:
             return x * self.a1 / self.b1
         if x < self.b2:
-            self.a1 + (self.a2 - self.a1) / (self.b2 - self.b1) * (x - self.b1)
+            return self.a1 + (self.a2 - self.a1) / (self.b2 - self.b1) * (x - self.b1)
         return self.a2
 
     def GetUtilityFunctionType(self):
@@ -93,16 +133,28 @@ class DoubleLinearUtilityFunction(UtilityFunction):
 
 class NormalCDFUtilityFunction(UtilityFunction):
     def __init__(self, params, performanceType):
+        if len(params) != 2:
+            raise ValueError("There must be two input parameters")
+        if np.isnan(params[0]) or params[0] < 0:
+            raise ValueError("Input parameter a must be a positive number")
+        if np.isnan(params[1]) or params[1] < 0:
+            raise ValueError("Input parameter b must be a positive number")
+
         self.a = params[0] # represents the max utility in the asymptotic limit
         self.b = params[1] # represents the point where 50% of the max utility is reached
         self.performanceType = performanceType
 
     def GetUtility(self, x):
         if np.isnan(x):
+            raise ValueError("The utility function input cannot be null")
+        if x < 0:
+            raise ValueError("The utility function only accepts positive values")
+
+        if np.isnan(x):
             return 0
         if x < 0:
             return 0
-        if x > 0:
+        if x >= 0:
             return self.a * 2 * (norm.cdf(0.675 * x / self.b)-0.5)
 
     def GetUtilityFunctionType(self):
